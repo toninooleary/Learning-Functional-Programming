3cc567970281e124daf523e768f12c1633825f65
Tonino Oleary <toninooleary@gmail.com>
2020-03-08 20:08:56 +0000

    Add information to README, Add declaritive programming example

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Learning-Functional-Programming
 Learning about functional programming and its distinctions from imperative programming
+
+# Streams
+- Streams can be used as an alternative to for loop operations on lists.
+
+## :
+
+- .filter will filter an imput. e.g. filter an expressiontakes a lambda as input.    
+        .filter(lambda expression)
+- .collect will collect any operations performed into a data type.
+        .collect(Collectors.toList())
+- .forEach will perform operations on these new list
+
+# 
--- functional_programming_project/src/main/java/imperative/main.java
+++ functional_programming_project/src/main/java/imperative/main.java
@@ -3,6 +3,7 @@ package imperative;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class main {
@@ -16,6 +17,7 @@ public class main {
             new Person("Jack", Gender.MALE)
         );
 
+        System.out.println("// Imperative approach");
         // Imperative approach (for finding out how many females we have)
         List<Person> females = new ArrayList<>();
 
@@ -28,6 +30,27 @@ public class main {
         for (Person female : females){
             System.out.println(female);
         }
+
+        System.out.println("// Declarative approach 1");
+        // Declarative Approach:
+        // This uses streams with collections
+        people.stream()
+                // filters all females
+                .filter(person -> Gender.FEMALE.equals(person.gender))
+                // stores output in a list
+                .collect(Collectors.toList())
+                // prints every element in the new list
+                .forEach(System.out::println);
+
+        System.out.println("// Declarative approach 2 (stored stream)");
+        //You can also store streams as lists
+        List<Person> fPeople = people.stream()
+                // filters all females
+                .filter(person -> Gender.FEMALE.equals(person.gender))
+                // stores output in a list
+                .collect(Collectors.toList());
+
+        fPeople.forEach(System.out::println);
 
     }
 
